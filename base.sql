d88753bdba9397b990847feedc5051a73738d204
Jorge Luis Juarez M <jjuarez007@gmail.com>
2014-07-21 07:58:31 -0500

    base v2

---
+++ base.sql
@@ -0,0 +1,439 @@
+-- MySQL dump 10.13  Distrib 5.6.16, for osx10.6 (x86_64)
+--
+-- Host: localhost    Database: rx
+-- ------------------------------------------------------
+-- Server version	5.6.16
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `answers`
+--
+
+DROP TABLE IF EXISTS `answers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `answers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `question_id` int(11) NOT NULL,
+  `answer` varchar(45) DEFAULT NULL,
+  `answer_is_ok` tinyint(1) DEFAULT NULL,
+  `value` decimal(18,2) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_answers_questions1_idx` (`question_id`),
+  CONSTRAINT `fk_answers_questions1` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `answers`
+--
+
+LOCK TABLES `answers` WRITE;
+/*!40000 ALTER TABLE `answers` DISABLE KEYS */;
+INSERT INTO `answers` VALUES (1,1,'5',1,10.00),(13,9,'Filmosis',0,0.00),(14,9,'Higiene deficiente',0,0.00),(15,9,'Tabaquismo',1,0.00),(16,9,'CircunsiciÃ³n',0,1.00),(17,9,'Ninguna de las anteriores',0,1.00),(20,1,'8',0,1.00);
+/*!40000 ALTER TABLE `answers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `calendar_events`
+--
+
+DROP TABLE IF EXISTS `calendar_events`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `calendar_events` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(150) NOT NULL,
+  `descripcion` text NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `calendar_events`
+--
+
+LOCK TABLES `calendar_events` WRITE;
+/*!40000 ALTER TABLE `calendar_events` DISABLE KEYS */;
+INSERT INTO `calendar_events` VALUES (2,'Prueba de Resistencia de Objeto','Prueba de Resistencia de Objeto','2014-07-20 10:30:42'),(3,'Prueba 2','prueba 2','2014-07-31 05:00:00');
+/*!40000 ALTER TABLE `calendar_events` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `exam_categories`
+--
+
+DROP TABLE IF EXISTS `exam_categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `exam_categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `exam_categories`
+--
+
+LOCK TABLES `exam_categories` WRITE;
+/*!40000 ALTER TABLE `exam_categories` DISABLE KEYS */;
+INSERT INTO `exam_categories` VALUES (1,'Matematicas II'),(2,'Matematicas I'),(3,'Matematicas III'),(4,'Fisica I'),(5,'Fisica II'),(6,'Fisica III');
+/*!40000 ALTER TABLE `exam_categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `exam_statuses`
+--
+
+DROP TABLE IF EXISTS `exam_statuses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `exam_statuses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `exam_statuses`
+--
+
+LOCK TABLES `exam_statuses` WRITE;
+/*!40000 ALTER TABLE `exam_statuses` DISABLE KEYS */;
+INSERT INTO `exam_statuses` VALUES (1,'iniciado'),(2,'concluido'),(3,'cancelado'),(4,'concluido');
+/*!40000 ALTER TABLE `exam_statuses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `exams`
+--
+
+DROP TABLE IF EXISTS `exams`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `exams` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `exam_category_id` int(11) NOT NULL,
+  `exam_description` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_exams_exam_categories1_idx` (`exam_category_id`),
+  CONSTRAINT `fk_exams_exam_categories1` FOREIGN KEY (`exam_category_id`) REFERENCES `exam_categories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `exams`
+--
+
+LOCK TABLES `exams` WRITE;
+/*!40000 ALTER TABLE `exams` DISABLE KEYS */;
+INSERT INTO `exams` VALUES (1,1,'Matematicas II 1er Parcial'),(2,2,'Matematicas I 1er Parcial');
+/*!40000 ALTER TABLE `exams` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `exams_questions`
+--
+
+DROP TABLE IF EXISTS `exams_questions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `exams_questions` (
+  `exam_id` int(11) NOT NULL,
+  `question_id` int(11) NOT NULL,
+  PRIMARY KEY (`exam_id`,`question_id`),
+  KEY `fk_exams_has_questions_questions1_idx` (`question_id`),
+  KEY `fk_exams_has_questions_exams1_idx` (`exam_id`),
+  CONSTRAINT `fk_exams_has_questions_exams1` FOREIGN KEY (`exam_id`) REFERENCES `exams` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_exams_has_questions_questions1` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `exams_questions`
+--
+
+LOCK TABLES `exams_questions` WRITE;
+/*!40000 ALTER TABLE `exams_questions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `exams_questions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `menus`
+--
+
+DROP TABLE IF EXISTS `menus`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `menus` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) DEFAULT NULL,
+  `child_menu` int(11) DEFAULT NULL,
+  `controller` varchar(45) DEFAULT NULL,
+  `action` varchar(45) DEFAULT NULL,
+  `class` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `menus`
+--
+
+LOCK TABLES `menus` WRITE;
+/*!40000 ALTER TABLE `menus` DISABLE KEYS */;
+INSERT INTO `menus` VALUES (1,'Home',0,'pages','display','fa fa-home'),(6,'Usuarios',0,'users','index','fa fa-user'),(7,'Lista de Usuarios',6,'users','index',NULL),(8,'Estadisticas de Usuarios',6,'users','charts',NULL),(9,'Examenes',0,'','','fa fa-edit'),(10,'Examenes (Templates)',9,'exams','index',''),(11,'Examenes (Usuario)',9,'user_exams','index',''),(12,'Preguntas',9,'questions','index','');
+/*!40000 ALTER TABLE `menus` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `menus_roles`
+--
+
+DROP TABLE IF EXISTS `menus_roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `menus_roles` (
+  `menu_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  PRIMARY KEY (`menu_id`,`role_id`),
+  KEY `fk_menus_has_roles_roles1_idx` (`role_id`),
+  KEY `fk_menus_has_roles_menus1_idx` (`menu_id`),
+  CONSTRAINT `fk_menus_has_roles_menus1` FOREIGN KEY (`menu_id`) REFERENCES `menus` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_menus_has_roles_roles1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `menus_roles`
+--
+
+LOCK TABLES `menus_roles` WRITE;
+/*!40000 ALTER TABLE `menus_roles` DISABLE KEYS */;
+INSERT INTO `menus_roles` VALUES (9,5),(10,5),(11,5),(12,5),(9,6),(10,6),(11,6),(12,6);
+/*!40000 ALTER TABLE `menus_roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `question_categories`
+--
+
+DROP TABLE IF EXISTS `question_categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `question_categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(150) NOT NULL,
+  `descripcion` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `question_categories`
+--
+
+LOCK TABLES `question_categories` WRITE;
+/*!40000 ALTER TABLE `question_categories` DISABLE KEYS */;
+INSERT INTO `question_categories` VALUES (1,'Cancer de Pene',''),(2,'Cancer Prostatico','');
+/*!40000 ALTER TABLE `question_categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `questions`
+--
+
+DROP TABLE IF EXISTS `questions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `questions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `question` text,
+  `question_category_id` int(11) NOT NULL,
+  `imagen` varchar(150) NOT NULL,
+  `question_status_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `questions`
+--
+
+LOCK TABLES `questions` WRITE;
+/*!40000 ALTER TABLE `questions` DISABLE KEYS */;
+INSERT INTO `questions` VALUES (1,'Raiz Cuadrada de 25',1,'cover.jpg',2),(2,'Area de un cuadrado de 8cm de lado',1,'imagen2.png',2),(8,'aaa',2,'22.JPG',2),(9,'Factores de riesgo para cÃ¡ncer de pene excepto:',1,'10482835_308922619273117_1321548922_n.jpg',2),(10,'Raiz Cuadrada de 25',1,'cover.jpg',0),(11,'Raiz Cuadrada de 25',1,'cover.jpg',0);
+/*!40000 ALTER TABLE `questions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (5,'Administrator'),(6,'Usuario');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles_users`
+--
+
+DROP TABLE IF EXISTS `roles_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `roles_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_user_has_role_users_idx` (`user_id`),
+  KEY `fk_user_has_role_roles1_idx` (`role_id`),
+  CONSTRAINT `fk_user_has_role_roles1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_has_role_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles_users`
+--
+
+LOCK TABLES `roles_users` WRITE;
+/*!40000 ALTER TABLE `roles_users` DISABLE KEYS */;
+INSERT INTO `roles_users` VALUES (1,1,5),(3,2,5);
+/*!40000 ALTER TABLE `roles_users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_answers`
+--
+
+DROP TABLE IF EXISTS `user_answers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_answers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_exam_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `question_id` int(11) NOT NULL,
+  `answer_id` int(11) NOT NULL,
+  `pregunta` varchar(45) NOT NULL,
+  `respuesta` varchar(45) NOT NULL,
+  `correcta` tinyint(1) NOT NULL,
+  `valor` decimal(18,2) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_user_answers_user_exams1_idx` (`user_exam_id`),
+  KEY `fk_user_answers_users1_idx` (`user_id`),
+  KEY `fk_user_answers_questions1_idx` (`question_id`),
+  KEY `fk_user_answers_answers1_idx` (`answer_id`),
+  CONSTRAINT `fk_user_answers_answers1` FOREIGN KEY (`answer_id`) REFERENCES `answers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_answers_questions1` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_answers_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_answers_user_exams1` FOREIGN KEY (`user_exam_id`) REFERENCES `user_exams` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_answers`
+--
+
+LOCK TABLES `user_answers` WRITE;
+/*!40000 ALTER TABLE `user_answers` DISABLE KEYS */;
+INSERT INTO `user_answers` VALUES (1,1,1,1,1,'Raiz Cuadrada de 25','5',1,10.00);
+/*!40000 ALTER TABLE `user_answers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_exams`
+--
+
+DROP TABLE IF EXISTS `user_exams`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_exams` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `exam_id` int(11) NOT NULL,
+  `last_answer` int(11) DEFAULT NULL,
+  `exam_status_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_user_exams_exams1_idx` (`exam_id`),
+  KEY `fk_user_exams_exam_statuses1_idx` (`exam_status_id`),
+  CONSTRAINT `fk_user_exams_exams1` FOREIGN KEY (`exam_id`) REFERENCES `exams` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_exams_exam_statuses1` FOREIGN KEY (`exam_status_id`) REFERENCES `exam_statuses` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_exams`
+--
+
+LOCK TABLES `user_exams` WRITE;
+/*!40000 ALTER TABLE `user_exams` DISABLE KEYS */;
+INSERT INTO `user_exams` VALUES (1,1,0,1);
+/*!40000 ALTER TABLE `user_exams` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Jahenn Darlaine','29c95bad258e20251c8dc5a08b3889c8daf74fd9'),(2,'jahenn','29c95bad258e20251c8dc5a08b3889c8daf74fd9');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-07-21  7:54:11
